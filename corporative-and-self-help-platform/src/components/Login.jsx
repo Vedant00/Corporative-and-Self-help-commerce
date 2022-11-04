3eb0ae4ceed4fd2373920cb2f4ea6c8c7c4b8950
Vedant Bhiwapure <vedant.21911171@viit.ac.in>
2022-11-04 22:25:31 +0530

    Log-in Change

--- corporative-and-self-help-platform/src/components/Login.jsx
+++ corporative-and-self-help-platform/src/components/Login.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-
 function Login(props){
     
-    return <div className="log-in">
+    return <div className="log-in-box">
+        <div className="log-in">
         <div class="form-floating mb-3">
   <input type="email" class="form-control" id="floatingInput" placeholder="Email address"/>
   <label for="floatingInput">Email address</label>
@@ -13,6 +13,14 @@ function Login(props){
   <label for="floatingPassword">Password</label>
 </div>
 <input class="btn btn-primary" type="Submit" value="Log In"></input>
+<div style={{height:"0.7px",width:"250px",backgroundColor:"rgb(191, 210, 228",marginBottom:"8px" }}></div>
+<div className="log-in-with" >
+<p className="log-in-with-txt">Log-In with:  <i class="fa-brands fa-google"></i> <i class="fa-brands fa-facebook"></i> </p>
+
+</div>
+      
+ </div>
+       
     </div>
 }
 
