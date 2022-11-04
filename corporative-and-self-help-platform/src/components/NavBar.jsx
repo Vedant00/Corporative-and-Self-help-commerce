971fa99b87fe9247f4183a2420a7d46db6c90e97
MayurGawate <gawate.mayur07@gmail.com>
2022-11-04 20:50:13 +0530

    navBar

--- corporative-and-self-help-platform/src/components/NavBar.jsx
+++ corporative-and-self-help-platform/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function NavBar(props){
   
     return <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style={{color: "red"}} >
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid" >
         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
