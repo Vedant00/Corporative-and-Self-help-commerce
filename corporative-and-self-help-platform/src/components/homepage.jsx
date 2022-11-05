64168ca849179184e554a5e419d775b0ba45f492
MayurGawate <gawate.mayur07@gmail.com>
2022-11-05 13:19:25 +0530

    orderpage

--- corporative-and-self-help-platform/src/components/homepage.jsx
+++ corporative-and-self-help-platform/src/components/homepage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Login from "./Login";
-
+import OrderPage from "./OrderPage";
 
 function Homepage(props){
+    const 
+
     return <div>
      <Login/>
+     {/* <OrderPage/> */}
         hello from homepage
     </div>
 }
