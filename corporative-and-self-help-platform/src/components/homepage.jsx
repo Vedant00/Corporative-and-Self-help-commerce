0cec0fd5a435edf01b6d162fd90ac7c24675cab2
Vedant Bhiwapure <vedant.21911171@viit.ac.in>
2022-11-08 18:45:17 +0530

    Card created

--- corporative-and-self-help-platform/src/components/homepage.jsx
+++ corporative-and-self-help-platform/src/components/homepage.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import Card from "./Cards";
 
+const names=['Naruto', 'Sauske', 'Sakaru', 'Kiba', 'Shino', 'Hinata', 'Shikamaru', 'Ino'];
+
 function Homepage(props){
-<<<<<<< HEAD
     return <div className="homepage">
-        <Card product="Rice" description="This is rice" price="80 ₹/kg"/>
-=======
-    const names=['Naruto', 'Sauske', 'Sakaru', 'Kiba', 'Shino', 'Hinata', 'Shikamaru', 'Ino'];
-    
-    return <div>
-        <ul>
             { names.map((name)=>{
-                return <li>{name}</li>
+                return  <Card product={name} description="This is rice" price="80 ₹/kg"/>
+
             })}
-        </ul>
->>>>>>> 8b9f2b753b2bc934f1d07afb2798a637358ecdc8
+
     </div>
 }
 export default Homepage;
