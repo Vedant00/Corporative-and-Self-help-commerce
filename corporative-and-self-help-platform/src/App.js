05284d3ed67c736cd1da93d42026b4d1095a664d
MayurGawate <gawate.mayur07@gmail.com>
2022-11-19 22:26:14 +0530

    shopping

--- corporative-and-self-help-platform/src/App.js
+++ corporative-and-self-help-platform/src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Homepage from './components/homepage';
-import Login from './components/Login';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./components/homepage";
+import Login from "./components/Login";
 import NavBar from "./components/NavBar";
-import SignUp from './components/SignUp';
-import OrderPage from './components/orderpage';
+import SignUp from "./components/SignUp";
+import OrderPage from "./components/orderpage";
+import Order from "./components/Order";
 function App() {
   return (
     <Router>
-      <NavBar/>
+      <NavBar />
       <Routes>
-          <Route exact path='/' element={<Homepage/>} />
-          <Route path='/signup' element={<SignUp/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/previousOrders' element={<OrderPage/>}/>
-          <Route element={<Homepage/>} />
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/previousOrders" element={<OrderPage />} />
+        <Route path="/Order" element={<Order />} />
+
+        <Route element={<Homepage />} />
       </Routes>
     </Router>
   );
