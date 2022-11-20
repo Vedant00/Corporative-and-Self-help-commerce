7323b89978e36a08f6db928810cd77a724c64916
MayurGawate <gawate.mayur07@gmail.com>
2022-11-20 22:40:57 +0530

    image

--- corporative-and-self-help-platform/src/components/Cards.jsx
+++ corporative-and-self-help-platform/src/components/Cards.jsx
@@ -1,46 +1,76 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import logo from "./favicon.ico";
 
-
-function Card(props){
-    const temp =props.package;
-    const [weight,setWeight]=useState("Weight");
-    function newWeight(weightin){
-        setWeight(weightin);
-    }
-    return <div className="card-container">
-         <div className="card">
-        <div className="card-image sticky-top"><img style={{width:"100px", objectFit:"fit"}} src={require('./favicon.ico') } /></div>
-        <div style={{width:"0.5px", height:"270px",backgroundColor:"black",margin:"20px 10px 20px 20px"}} className="sticky-top"></div>
+function Card(props) {
+  const temp = props.package;
+  const [weight, setWeight] = useState("Weight");
+  function newWeight(weightin) {
+    setWeight(weightin);
+  }
+  console.log(props.image);
+  return (
+    <div className="card-container">
+      <div className="card">
+        <div className="card-image sticky-top">
+          <img
+            style={{ width: "100px", objectFit: "fit" }}
+            src={props.image}
+            alt={props.name}
+          />
+        </div>
+        <div
+          style={{
+            width: "0.5px",
+            height: "270px",
+            backgroundColor: "black",
+            margin: "20px 10px 20px 20px",
+          }}
+          className="sticky-top"
+        ></div>
         <div className="card-items">
-            <p>{props.product}</p>
-            <p>{props.description}</p>
-            <p>Price : {props.price} ₹/ {props.unit}</p>
+          <p>{props.product}</p>
+          <p>{props.description}</p>
+          <p>
+            Price : {props.price} ₹/ {props.unit}
+          </p>
 
-          
-        <div class="dropdown">
-         <button  className="cart-btn btn btn-secondary btn-sm dropdown-toggle w-50 " type="button" data-bs-toggle="dropdown" aria-expanded="false">
-               {weight}
+          <div className="dropdown">
+            <button
+              className="cart-btn btn btn-secondary btn-sm dropdown-toggle w-50 "
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {weight}
             </button>
             <ul class="dropdown-menu">
-            
-            {temp.map((pack)=>{
-                return<li><a class="dropdown-item" href="#" onClick={()=>{newWeight({pack})}} value="1kg">{pack} KG</a></li>
-            })}
-             </ul>
-            </div>
-            <div className="quantity">
+              {temp.map((pack) => {
+                return (
+                  <li>
+                    <div
+                      class="dropdown-item"
+                      onClick={() => {
+                        newWeight({ pack });
+                      }}
+                    >
+                      {pack} KG
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="quantity">
             <p>Quantity :</p>
-            <input className="quantity-amt" placeholder="1"/>
-            </div>
-            <button type="button" class="add-cart btn btn-primary">Add to Cart</button>
+            <input className="quantity-amt" placeholder="1" />
+          </div>
+          <button type="button" class="add-cart btn btn-primary">
+            Add to Cart
+          </button>
         </div>
-        </div>
-       
-    </div> 
-    
-   
-    
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
