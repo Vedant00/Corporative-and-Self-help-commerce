c818069012cee9f575597dd3098379b1e2dd1816
Vedant Bhiwapure <vedant.21911171@viit.ac.in>
2022-12-13 00:06:05 +0530

    SignUp database, password hashed, SignIn validator -  added

--- corporative-and-self-help-platform/src/server/Router/auth.js
+++ corporative-and-self-help-platform/src/server/Router/auth.js
@@ -12,10 +12,48 @@ router.get('/signup',(req,res)=>{
     res.send("Hello on Signup")
 });
 
-router.post('/signup',(req,res)=>{
+router.post('/signup',async (req,res)=>{
     const { fname, lname, email, password, cpassword }=req.body;
-    console.log(fname);
-    res.json({message:req.body});
+   
+    if (!fname || !lname || !email || !password || !cpassword){
+        return res.status(422).json({error:"Please fill all fields correctly"});
+    }
+    try{
+       const userExist = await User.findOne({email:email});
+       if(userExist){
+        return res.status(422).json({error:"User already exist"});
+       }else if(password != cpassword){
+            return res.status(422).json({err:"passwords are not matching"});
+       }
+
+       const user =new User({fname, lname, email, password, cpassword});
+
+
+       await  user.save();
+       
+        res.status(201).json({message:"new user created"})
+    }catch(err){
+        console.log(err);
+    }
+});
+
+router.post('/signin', async(req,res)=>{
+    try{
+        const [email , password]=req.body;
+
+        if(!email || ! password){
+            return res.status(400).json({error:"Please enter credentials"})
+        }
+
+        const inputUser= await User.findOne({email:email})
+        if(!inputUser){
+            res.status(400).json({error:"No user found"})
+        }else{
+            res.json({message:"user signin sucessful"});
+        }
+    }catch(err){
+        console.log(err);
+    }
 });
 
 module.exports = router;
